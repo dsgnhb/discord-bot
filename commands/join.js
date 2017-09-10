2a7c01cacec35058f312111e8fabf999121bcd0c
Daniel Heyne <6844603+derheyne@users.noreply.github.com>
2017-09-10 18:33:03 +0200

    Did things ...
    Updated formatting, fixed typos and switched messages when joining and leaving a skill-group.

--- commands/join.js
+++ commands/join.js
@@ -1,36 +1,50 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-lets
     const rank = args[0];
-    if(!rank) return message.channel.send("Nutze `!join <role>` um eine Skill-Gruppe zu joinen.");
-    const skillGroups = ["css", "html", "affinityphoto", "ruby", "sql", "c++", "c#", "swift", "phyton", "java", "php", "javascript", "blender", "cinema4d", "gimp", "photoshop"];
-    if(skillGroups.indexOf(rank.toLowerCase()) == -1) return;
+    const skillGroups = [
+        `css`, `html`, `affinityphoto`, `ruby`, `sql`, `c++`, `c#`, `swift`, `phyton`, `java`, `php`, `javascript`,
+        `blender`, `cinema4d`, `gimp`, `photoshop`,
+    ];
+
+    if (!rank) {
+        return message.channel.send(`Nutze \`!join <role>\` um einer Skill-Gruppe zu joinen.`);
+    }
+    
+    if (skillGroups.indexOf(rank.toLowerCase()) === -1) {
+        return;
+    }
+    
     role = message.guild.roles.find(r => r.name.toLowerCase() === rank.toLowerCase());
-    if (!role) return;
-    if(message.member.roles.has(role.id)) {
-      message.member.removeRole(role, "Requested via !join.").then(() =>{
-        message.channel.send("Du wurdest zur Gruppe **" + role.name + "** hinzugefügt.")
-      }).catch((error) => {
-        // No Perms
-      })
-      
-    } else {
-        message.member.addRole(role, "Requested via !join.").then(() =>{
-          message.channel.send("Du wurdest aus der **Gruppe " + role.name + "** entfernt.")
+    
+    if (!role) {
+        return;
+    }
+    
+    if (message.member.roles.has(role.id)) {
+        message.member.removeRole(role, `Requested via !join.`).then(() => {
+            message.channel.send(`Du wurdest aus der **Gruppe ${role.name}** entfernt.`)
         }).catch((error) => {
-          // No Perms
+            // No Perms
+        })
+
+    } else {
+        message.member.addRole(role, `Requested via !join.`).then(() => {
+            message.channel.send(`Du wurdest zur Gruppe **${role.name}** hinzugefügt.`)
+        }).catch((error) => {
+            // No Perms
         })
     }
-  }
-  
-  exports.conf = {
+};
+
+exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
     permLevel: 0
-  };
-  
-  exports.help = {
-    name: "join",
-    category: "Utility",
-    description: "Bekomme nen Rang ohne nen Admin vollzuspamen. - voll schlau von mir",
-    usage: "join <role>"
-  };
+};
+
+exports.help = {
+    name: `join`,
+    category: `Utility`,
+    description: `Bekomme nen Rang ohne nen Admin vollzuspamen. - voll schlau von mir`,
+    usage: `join <role>`
+};
