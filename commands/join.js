1bb98af49e1179d035428f84cba8da6ab47e879f
CreepPlaysDE <alex@apollodesign.de>
2017-09-10 21:52:09 +0200

    End of debug join.js

--- commands/join.js
+++ commands/join.js
@@ -1,21 +1,15 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-lets
-    console.log("args.length = " + args.length);
     if(args.length === 0) return message.channel.send("Nutze `!join <role>` um eine Skill-Gruppe zu joinen.");
     const skillGroups = ["css", "html", "affinityphoto", "ruby", "sql", "c++", "c#", "swift", "phyton", "java", "php", "javascript", "blender", "cinema4d", "gimp", "photoshop"];
     let addedRanks = [], removedRanks = [];
     for(let rank of args) {
-        console.log(rank);
-        console.log("Rang-Schleife - Anfang");
         if (skillGroups.indexOf(rank.toLowerCase()) === -1) continue;
-        console.log("Rang-Schleife - nach Skillgroupscheck")
         role = message.guild.roles.find(r => r.name.toLowerCase() === rank.toLowerCase());
         if (!role) return;
-        console.log("Rolle existiert");
         if(message.member.roles.has(role.id)) {
             try {
                 await message.member.removeRole(role, "Requested via !join.");
                 removedRanks.push(role);
-                console.log("Rolle entfernt");
             } catch(ex) {
                 // No perms
             }
@@ -23,7 +17,6 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
             try {
                 await message.member.addRole(role, "Requested via !join.");
                 addedRanks.push(role);
-                console.log("Rolle hinzugefügt");
             } catch(ex) {
                 // No perms
             }
@@ -31,11 +24,10 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     }
 
     if(addedRanks.length > 0) {
-        console.log("Nachricht senden");
-        return message.channel.send("Du wurdest zu den Gruppe(n) **" + addedRanks.join(", ") + "** hinzugefügt.");
-    } else if(removedRanks.length > 0) {
-        console.log("Nachricht senden");
-        return message.channel.send("Du wurdest aus den Gruppe(n) **" + removedRanks.join(", ") + "** entfernt.");
+        message.channel.send("Du wurdest zu den Gruppe(n) **" + addedRanks.join(", ") + "** hinzugefügt.");
+    }
+    if(removedRanks.length > 0) {
+        message.channel.send("Du wurdest aus den Gruppe(n) **" + removedRanks.join(", ") + "** entfernt.");
     }
   };
   
