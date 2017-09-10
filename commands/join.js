28fd60cbaec5c1c163d1d2869cf28b075cb2a67e
CreepPlaysDE <alex@apollodesign.de>
2017-09-10 18:59:07 +0200

    Made join command array-capable

--- commands/join.js
+++ commands/join.js
@@ -1,25 +1,33 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-lets
-    const rank = args[0];
-    if(!rank) return message.channel.send("Nutze `!join <role>` um eine Skill-Gruppe zu joinen.");
+    if(args.length === 0) return message.channel.send("Nutze `!join <role>` um eine Skill-Gruppe zu joinen.");
     const skillGroups = ["css", "html", "affinityphoto", "ruby", "sql", "c++", "c#", "swift", "phyton", "java", "php", "javascript", "blender", "cinema4d", "gimp", "photoshop"];
-    if(skillGroups.indexOf(rank.toLowerCase()) == -1) return;
-    role = message.guild.roles.find(r => r.name.toLowerCase() === rank.toLowerCase());
-    if (!role) return;
-    if(message.member.roles.has(role.id)) {
-      message.member.removeRole(role, "Requested via !join.").then(() =>{
-        message.channel.send("Du wurdest zur Gruppe **" + role.name + "** hinzugefügt.")
-      }).catch((error) => {
-        // No Perms
-      })
-      
-    } else {
-        message.member.addRole(role, "Requested via !join.").then(() =>{
-          message.channel.send("Du wurdest aus der **Gruppe " + role.name + "** entfernt.")
-        }).catch((error) => {
-          // No Perms
-        })
+    let addedRanks = [], removedRanks = [];
+    for(let rank in args) {
+        if (skillGroups.indexOf(rank.toLowerCase()) === -1) continue;
+        role = message.guild.roles.find(r => r.name.toLowerCase() === rank.toLowerCase());
+        if (!role) return;
+        if(message.member.roles.has(role.id)) {
+            message.member.removeRole(role, "Requested via !join.").then(() =>{
+                removedRanks.push(role);
+            }).catch((error) => {
+                // No Perms
+            })
+        } else {
+            message.member.addRole(role, "Requested via !join.").then(() =>{
+                addedRanks.push(role);
+            }).catch((error) => {
+                // No Perms
+            })
+        }
     }
-  }
+
+    if(addedRanks.length > 0) {
+        message.channel.send("Du wurdest zu den Gruppe(n) **" + addedRanks.join(", ") + "** hinzugefügt.");
+    }
+    if(removedRanks.length > 0) {
+        message.channel.send("Du wurdest aus den Gruppe(n) **" + removedRanks.join(", ") + "** entfernt.");
+    }
+  };
   
   exports.conf = {
     enabled: true,
