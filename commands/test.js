261c59035f6272407df61e786257c8021b8f2c20
Flo <x.client.ich@gmail.com>
2017-09-10 22:00:58 +0200

    Added test command (No function)

---
+++ commands/test.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-lets
+
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 10
+};
+
+exports.help = {
+    name: "test",
+    category: "test",
+    description: "Test Command.",
+    usage: "test"
+};
