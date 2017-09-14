e203b347219de2c59f723ffd3bf33f23769cf671
lukasc4d <31734055+lukasc4d@users.noreply.github.com>
2017-09-14 16:06:26 +0200

    owner /gommemode
    jetzt klappts

--- monitors/gommemode.js
+++ monitors/gommemode.js
@@ -1,8 +1,7 @@
 exports.run = (client, message, level) => {
     if (message.content.startsWith("/gommemode")) {
-      const functions = require("../functions/functions.js");
       let role = message.guild.roles.find(r => r.name === "/gommemode");
-      if(!message.member.roles.has(role.id)||functions.permlevel(message)<9) return;
+      if(!message.member.roles.has(role.id)||client.permlevel(message)<9) return;
       message.channel.send("**trololol!**");
       client.log("log", `${message.author.username} (${message.author.id}) ran command /gommemode`, "CMD")
     }
