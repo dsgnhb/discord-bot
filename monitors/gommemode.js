1152e776d04c930a19a2cbaa3834577e799d45d7
lukasc4d <31734055+lukasc4d@users.noreply.github.com>
2017-09-14 16:09:03 +0200

    JETZT KLAPPTS

--- monitors/gommemode.js
+++ monitors/gommemode.js
@@ -1,7 +1,8 @@
 exports.run = (client, message, level) => {
     if (message.content.startsWith("/gommemode")) {
+      console.log(level);
       let role = message.guild.roles.find(r => r.name === "/gommemode");
-      if(!message.member.roles.has(role.id)||client.permlevel(message)<9) return;
+      if(!message.member.roles.has(role.id)||level<9) return;
       message.channel.send("**trololol!**");
       client.log("log", `${message.author.username} (${message.author.id}) ran command /gommemode`, "CMD")
     }
