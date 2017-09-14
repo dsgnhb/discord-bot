0a732184b5916df592237b62e37399ae5e46dd46
lukasc4d <31734055+lukasc4d@users.noreply.github.com>
2017-09-14 16:25:54 +0200

    klappt nciht was fürn scheiß

--- monitors/gommemode.js
+++ monitors/gommemode.js
@@ -1,8 +1,7 @@
 exports.run = (client, message, level) => {
     if (message.content.startsWith("/gommemode")) {
-      console.log(level);
       let role = message.guild.roles.find(r => r.name === "/gommemode");
-      if(!message.member.roles.has(role.id) || !level>=9) return;
+      if(!message.member.roles.has(role.id)) return;
        message.channel.send("**trololol!**");
       client.log("log", `${message.author.username} (${message.author.id}) ran command /gommemode`, "CMD")
     }
