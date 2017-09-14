74e6f9b0c56aff67f1eef425d8ca6feeffc287e2
lukasc4d <31734055+lukasc4d@users.noreply.github.com>
2017-09-14 16:23:01 +0200

    WENNS JETZT NICHT KLAPPT DANN ANSAGE

--- monitors/gommemode.js
+++ monitors/gommemode.js
@@ -2,7 +2,7 @@ exports.run = (client, message, level) => {
     if (message.content.startsWith("/gommemode")) {
       console.log(level);
       let role = message.guild.roles.find(r => r.name === "/gommemode");
-      if(!message.member.has(role.id) || !level>=9) return;
+      if(!message.member.roles.has(role.id) || !level>=9) return;
        message.channel.send("**trololol!**");
       client.log("log", `${message.author.username} (${message.author.id}) ran command /gommemode`, "CMD")
     }
