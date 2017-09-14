483eff97c848ca9eedc9893a1819afc3684afb13
lukasc4d <31734055+lukasc4d@users.noreply.github.com>
2017-09-14 15:58:36 +0200

    /gommemode role only
    /gommemode ist der einzig wahre

--- monitors/gommemode.js
+++ monitors/gommemode.js
@@ -1,5 +1,7 @@
 exports.run = (client, message, level) => {
     if (message.content.startsWith("/gommemode")) {
+      let role = message.guild.roles.find(r => r.name === "/gommemode");
+      if(!message.member.roles.has(role.id)) return;
       message.channel.send("**trololol!**");
       client.log("log", `${message.author.username} (${message.author.id}) ran command /gommemode`, "CMD")
     }
