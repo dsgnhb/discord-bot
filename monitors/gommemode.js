8de4433030b8ae6a0d22c91da906c6b5f98013ee
lukasc4d <31734055+lukasc4d@users.noreply.github.com>
2017-09-14 16:04:30 +0200

    owner /gommemode
    ja klappt bestimmt

--- monitors/gommemode.js
+++ monitors/gommemode.js
@@ -1,7 +1,8 @@
 exports.run = (client, message, level) => {
     if (message.content.startsWith("/gommemode")) {
+      const functions = require("../functions/functions.js");
       let role = message.guild.roles.find(r => r.name === "/gommemode");
-      if(!message.member.roles.has(role.id)) return;
+      if(!message.member.roles.has(role.id)||functions.permlevel(message)<9) return;
       message.channel.send("**trololol!**");
       client.log("log", `${message.author.username} (${message.author.id}) ran command /gommemode`, "CMD")
     }
