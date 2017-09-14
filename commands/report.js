0ab338628e731967be788a41649b6e10cb43b19c
lukasc4d <31734055+lukasc4d@users.noreply.github.com>
2017-09-14 16:38:52 +0200

    joa

--- commands/report.js
+++ commands/report.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     const errorcode = Math.floor((Math.random() * 100000) + 1);
     message.channel.send("Vielen Dank! Dein Report wurde entgegengenommen. \nDie Gomme-Mods werden den Vorfall überprüfen! <:gomme:313418733861470210>")
     await client.wait(10000)
-    let messages = ["Puuh! Die Gomme-Mods sind am schwitzen und scheinen hier etwas überfordert."]
+    let messages = ["Puuh! Die Gomme-Mods sind am schwitzen und scheinen hier etwas überfordert.", "Leider mussten alle Gomme-Mods schon ins Bett.", "Alle Gomme-Mods sind gerade am kacken. Versuchs in "+errorcode + " Stunden nochmal. loool"]
     var msg = messages[Math.floor(Math.random()*messages.length)];
     message.channel.send(msg+" \nUnbekannter Error: x" +  + errorcode)
 }
