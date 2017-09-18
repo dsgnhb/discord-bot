860ec478f6f1dbbb75928ea79193a9f63c4a7519
lukasc4d <31734055+lukasc4d@users.noreply.github.com>
2017-09-18 20:00:04 +0200

    Gomme Video
    loool

--- commands/report.js
+++ commands/report.js
@@ -1,22 +1,13 @@
-const request= require("request");
+const fun = require("../functions/fun.js");
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-lets
-    if(!args[0]) return;
-    const errorcode = Math.floor((Math.random() * 100000) + 1);
-    message.channel.send("Vielen Dank! Dein Report wurde entgegengenommen. \nDie Gomme-Mods werden den Vorfall überprüfen! <:gomme:313418733861470210>");
-    /*request.get({
-       url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC7MIk4LVEjcGiJX919oJ1Eg&maxResults=10&order=date&type=video&key=AIzaSyCfrC4r1SgdL8XMd8JYBox9BePIhDAeOTs",
-       json: true
-    }, function(error, response, body) {
-        let video = body.items[0].id.videoId;
-        console.log(video);
-       // message.channel.send(video);
-        });*/
-    await client.wait(10000)
-    let messages = ["Puuh! Die Gomme-Mods sind am schwitzen und scheinen hier etwas überfordert.", "Leider mussten alle Gomme-Mods schon ins Bett.", "Alle Gomme-Mods sind gerade am kacken. Versuchs in "+errorcode + " Stunden nochmal. loool", "Error 404, welcher Vollidiot hat den TS heruntergefahren?"]
-    var msg = messages[Math.floor(Math.random()*messages.length)];
-    message.channel.send(msg+" \nUnbekannter Error: x" +  + errorcode)
-
+            if(!args[0]) return;
+          const errorcode = Math.floor((Math.random() * 100000) + 1);
+          message.channel.send("Vielen Dank! Dein Report wurde entgegengenommen. \nDie Gomme-Mods werden den Vorfall überprüfen! <:gomme:313418733861470210>");
+            await client.wait(10000);
+          let messages = ["Puuh! Die Gomme-Mods sind am schwitzen und scheinen hier etwas überfordert.", "Leider mussten alle Gomme-Mods schon ins Bett.", "Alle Gomme-Mods sind gerade am kacken. Versuchs in "+errorcode + " Stunden nochmal. loool", "Error 404, welcher Vollidiot hat den TS heruntergefahren?", "Oh! Wir schauen hier gerade das neuste Video von Gomme. Guck doch mit: "+ await fun.gommeVideo() + "!"]
+          var msg = messages[Math.floor(Math.random()*messages.length)];
+          message.channel.send(msg+" \nUnbekannter Error: x" +  + errorcode)
 };
 
 exports.conf = {
