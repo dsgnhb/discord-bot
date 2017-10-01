e4c7c7a53cb186faaf9d645d597ec4b51a7936f7
Flo <x.client.ich@gmail.com>
2017-10-01 20:58:01 +0200

    React 2

--- monitors/react.js
+++ monitors/react.js
@@ -2,11 +2,11 @@ exports.run = async (client, message, level) => {
   const reactions = {
     Marianne: {
       input: [ 'sahnekönigin', 'halt stop', 'jetzt hälst du die schnauze', 'sonst gibt krieg', 'käsefreund', 'sag ich dir gleich', 'es ist obst im haus', 'das bleibt alles so', 'ob du hier bist', 'beschweren kann man sich da eher weniger', 'schokolade', 'dome' ],
-      output: [ 'was den hier los?!!', 'was das den hier??!', 'halt stop!', 'was machst du denn??!' ]
+      output: [ 'Was den HIER los?!!', 'Was das den HIER??!', 'Was machst du DENN??!' ]
     },
     Andreas: {
       input: [ 'ich hab kein obst gefunden' ],
-      output: [ 'das kinderzimmer ist sauber', 'halt stop' ]
+      output: [ 'Das Kinderzimmer ist sauber!', 'Halt stop' ]
     },
     GommeHD: {
       input: [ '/gommemode' ],
@@ -21,11 +21,10 @@ exports.run = async (client, message, level) => {
   let possibleKeys = Object.keys(reactions).filter(k => reactions[k].input.some(word => message.content.toLowerCase().includes(word)))
   if (possibleKeys.length > 0) {
     let name = possibleKeys[0]
-    const randomOutput = reactions[name].output[Math.floor(Math.random() * reactions[name].output.length)].toUpperCase()
+    const randomOutput = reactions[name].output[Math.floor(Math.random() * reactions[name].output.length)]
     message.guild.members.get(client.user.id).setNickname(name)
-    message.channel.send(randomOutput)
+    await message.channel.send(randomOutput)
     client.log('log', `${name} reacted on ${message.author.username}'s (${message.author.id}) message with answer "${randomOutput}"`, 'React')
-    await client.wait(2000)
     message.guild.members.get(client.user.id).setNickname('dsgnhb')
   }
 }
