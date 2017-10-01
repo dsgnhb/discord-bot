1a231a4e384adbe16c492a8a80036ba4916e5884
CreepPlaysDE <alex@apollodesign.de>
2017-10-01 20:48:45 +0200

    Add multiple reaction types

--- monitors/marianereacts.js
+++ monitors/marianereacts.js
@@ -1,20 +1,31 @@
 exports.run = async (client, message, level) => {
   // Forbidden Words
-  const reactOn = ['sahnekönigin', 'halt stop', 'jetzt hälst du die schnauze', 'sonst gibt krieg', 'käsefreund', 'sag ich dir gleich', 'es ist obst im haus', 'das bleibt alles so', 'ob du hier bist', 'beschweren kann man sich da eher weniger']
-  const answers = ['was den hier los?!!', 'was das den hier??!', 'halt stop!', 'was machst du denn??!']
-  if (reactOn.some(word => message.content.toLowerCase().includes(word))) {
-    const random = answers[Math.floor(Math.random() * answers.length)].toUpperCase()
-    message.guild.members.get(client.user.id).setNickname('Marianne')
-    message.channel.send('Marianne: ' + random)
-    client.log('log', `Marianne reacted on ${message.author.username}'s (${message.author.id}) message with answer "${random}"`, 'MarianeReacts')
-    await client.wait(2000)
-    message.guild.members.get(client.user.id).setNickname('dsgnhb')
+  const reactions = {
+      Marianne : {
+          input : [ 'sahnekönigin', 'halt stop', 'jetzt hälst du die schnauze', 'sonst gibt krieg', 'käsefreund', 'sag ich dir gleich', 'es ist obst im haus', 'das bleibt alles so', 'ob du hier bist', 'beschweren kann man sich da eher weniger', 'schokolade', 'dome' ],
+          output :  [ 'was den hier los?!!', 'was das den hier??!', 'halt stop!', 'was machst du denn??!' ],
+      },
+      Andreas : {
+          input : [ 'ich hab kein obst gefunden' ],
+          output :  [ 'das kinderzimmer ist sauber', 'halt stop' ],
+      }
+  };
+
+  let possibleKeys = Object.keys(reactions).filter(k => reactions[k].input.some(word => message.content.toLowerCase().includes(word)));
+  if(possibleKeys.length > 0) {
+      let name = possibleKeys[0];
+      const random = reactions[name].output[Math.floor(Math.random() * reactions[name].output.length)].toUpperCase();
+      message.guild.members.get(client.user.id).setNickname(name);
+      message.channel.send(name + ': ' + random)
+      client.log('log', `${name} reacted on ${message.author.username}'s (${message.author.id}) message with answer "${random}"`, 'MarianeReacts')
+      await client.wait(2000);
+      message.guild.members.get(client.user.id).setNickname('dsgnhb')
   }
-}
+};
 exports.conf = {
   enabled: true,
   guildOnly: true
-}
+};
 
 exports.help = {
   name: 'MarianeReacts',
