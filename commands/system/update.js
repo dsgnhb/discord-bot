7e5e081b95a3fa7d10c2a5058227fa0a7700d193
Flo <x.client.ich@gmail.com>
2017-10-22 15:17:10 +0200

    2.0.3

--- commands/system/update.js
+++ commands/system/update.js
@@ -3,6 +3,7 @@ const Command = require('../../base/commands/Command.js')
 const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
 const path = require('path')
+const { RichEmbed } = require('discord.js')
 
 class Update extends Command {
   constructor(client) {
@@ -19,15 +20,36 @@ class Update extends Command {
   }
 
   async run(message, args) {
-    const { stdout, stderr, err } = await exec(`git pull ${require('../../package.json').repository.url.split('+')[1]}`, { cwd: path.join(__dirname, '../../') }).catch(err => ({ err }))
+    const settings = message.settings
+
+    const repository = require('../../package.json').repository.url.split('+')[1]
+    delete require.cache[require.resolve('../../package.json')]
+
+    const { stdout, stderr, err } = await exec(`git pull ${repository}`, { cwd: path.join(__dirname, '../../') }).catch(err => ({ err }))
     if (err) return console.error(err)
+
+    if (stdout.toString().includes('Already up-to-date.')) return message.channnel.send('Already up-to-date.')
+
+    const changelog = require('../../changelog.json')
+    const newVersion = require('../../package.json').version
+    await message.channel.send(
+      new RichEmbed()
+        .setAuthor('Changelog')
+        .setURL(repository.slice(0, -4))
+        .setColor(settings.embedColor)
+        .addField(`v${NewVersion}`, chnagelog[newVersion].join('\n'), false)
+        .setTimestamp()
+        .setFooter(settings.embedFooter, settings.embedIcon)
+    )
+
+    /*
     const out = []
     if (stdout) out.push(stdout)
     if (stderr) out.push(stderr)
     await message.channel.send(out.join('---\n'), { code: true })
-    if (!stdout.toString().includes('Already up-to-date.')) {
-      this.client.commands.get('reboot').run(message, args)
-    }
+    */
+
+    this.client.commands.get('reboot').run(message, args)
   }
 }
 
