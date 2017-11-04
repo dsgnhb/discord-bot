ed6afce247378628dfe2c498d0f19f089239d6a7
Flo <x.client.ich@gmail.com>
2017-11-04 17:58:56 +0100

    Gifs in !show

--- commands/topdesign/show.js
+++ commands/topdesign/show.js
@@ -19,7 +19,9 @@ class Show extends TopDesignCommand {
     try {
       const request = await this.f.getPost(postID)
       if (!request) return message.channel.send(`**TopDesign** | Der Post mit der Nummer **#${postID}** konnte nicht gefunden werden.`)
-      message.channel.send(`**TopDesign** | ${request.username} - **${request.likes}** ${this.f.voteOrVotes(request.likes)}`, new Attachment(request.image, 'design.jpg'))
+      const imageArray = request.image.split('.')
+      const extention = imageArray[imageArray.length - 1]
+      message.channel.send(`**TopDesign** | ${request.username} - **${request.likes}** ${this.f.voteOrVotes(request.likes)}`, new Attachment(request.image, 'design.' + extention))
     } catch (error) {
       console.log(error)
       message.channel.send('**TopDesign** | Uiih. hier scheint etwas nicht zu funktionieren, wie es sollte.. 😕')
