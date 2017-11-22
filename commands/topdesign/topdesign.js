36dc507135debad77abfe4fbfbec122ccb66cac5
Flo <x.client.ich@gmail.com>
2017-11-22 12:41:22 +0100

    vids in !topdesign and !vote

--- commands/topdesign/topdesign.js
+++ commands/topdesign/topdesign.js
@@ -12,9 +12,7 @@ class Topdesign extends TopDesignCommand {
   }
 
   async run(message, args) {
-    message.channel.send(
-      '**TopDesign** | Füge den Hashtag `#topdesign` zu deinem Design hinzu, um es für die Bewertung freizugeben. \nNutze `!vote` um einen Post zu liken, `!delete` um ihn zu löschen oder `!posts` für eine Übersicht alles Posts.'
-    )
+    throw 'Füge den Hashtag `#topdesign` zu deinem Design hinzu, um es für die Bewertung freizugeben. \nNutze `!vote` um einen Post zu liken oder `!posts` für eine Übersicht alles Posts. \nhttps://youtu.be/Ei_QI8gbkss'
   }
 }
 
--- commands/topdesign/vote.js
+++ commands/topdesign/vote.js
@@ -15,17 +15,18 @@ class Vote extends TopDesignCommand {
   async run(message, args) {
     if (args[0] === '<:gomme:313418733861470210>') return message.channel.send(`Vielen Dank für deine Stimme! ${this.client.emojis.get('313418733861470210')}`)
     const postID = this.f.getPostID(args)
-    if (!postID) return message.channel.send('**TopDesign** | Nutze `!vote #[Nr des Posts]` um für einen Post zu voten.')
+    if (!postID) throw 'Nutze `!vote #[Nr des Posts]` um für einen Post zu voten. \nhttps://youtu.be/trJqJfpdReE'
     try {
       const request = await this.f.votePost(postID, message.author.id)
-      if (!request) return message.channel.send(`**TopDesign** | Der Post mit der Nummer **#${postID}** konnte nicht gefunden werden.`)
+      if (!request) throw `**TopDesign** | Der Post mit der Nummer **#${postID}** konnte nicht gefunden werden.`
       if (request.action === 'remove')
-        return message.channel.send(`**TopDesign** | Dein Vote wurde entfernt! Der Post von **${request.posted_by}** hat jetzt **${request.likes} ${this.f.voteOrVotes(request.likes)}**.`)
+        return message.channel.send(`Dein Vote wurde entfernt! Der Post von **${request.posted_by}** hat jetzt **${request.likes} ${this.f.voteOrVotes(request.likes)}**.`)
       if (request.action === 'add')
-        return message.channel.send(`**TopDesign** | Dein Vote wurde hinzugefügt! Der Post von **${request.posted_by}** hat jetzt **${request.likes} ${this.f.voteOrVotes(request.likes)}**.`)
+        return message.channel.send(`Dein Vote wurde hinzugefügt! Der Post von **${request.posted_by}** hat jetzt **${request.likes} ${this.f.voteOrVotes(request.likes)}**.`)
     } catch (error) {
       console.log(error)
-      message.channel.send('**TopDesign** | Uiih. hier scheint etwas nicht zu funktionieren, wie es sollte.. 😕')
+      throw error
+      // message.channel.send('**TopDesign** | Uiih. hier scheint etwas nicht zu funktionieren, wie es sollte.. 😕')
     }
   }
 }
