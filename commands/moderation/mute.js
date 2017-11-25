b2d01d1244a1419d1af581ecbeb68c3f0b9f5f57
Flo <x.client.ich@gmail.com>
2017-11-25 11:39:42 +0100

    MUTE

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -15,25 +15,43 @@ class Mute extends Command {
   }
 
   async run(message, args) {
+    const muteRoleName = 'Muted'
     let toMute = message.guild.member(message.mentions.users.first())
+    let toMuteMember = message.guild.members.get(toMute.id) || message.guild.members.get(args[0])
     if (!toMute) return message.channel.send('Wen soll ich muten? Bin ja schon ganz aufgeregt ;)')
     if (toMute.id === message.author.id) return message.channel.send('Das bist doch du xd')
-    const muteRole = message.guild.roles.find('name', 'Muted')
-    if (!muteRole) return message.channel.send('DREGGS SERVER! HIER GIBST NEDMAL NE MUTE ROLE!')
-    const time = args[1] || 60 * 1000
+    let muteRole = await message.guild.roles.find(r => r.name === muteRoleName)
+    if (!muteRole) {
+      try {
+        muteRole = await message.guild.createRole({
+          name: muteRoleName,
+          color: 'black'
+        })
+        console.log(`Created role ${muteRole.name}`)
+
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muteRole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          })
+        })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    const time = args[1] * 1000 || 60 * 1000
 
     if (toMute.roles.has(muteRole.id)) {
       toMute.removeRole(muteRole)
-      return message.channel.send(`${toMute} wurde entmutet!`)
+      return message.reply(`Endlich! ${toMute} wurde entmutet!`)
+    } else {
+      await toMute.addRole(muteRole)
+      message.reply(`WapBap! ${toMute} wurde für ${ms(time, { long: true })} gemutet!`)
     }
-
-    await toMute.addRole(muteRole)
-    message.channel.send(`WapBap! ${toMute} wurde für ${ms(ms(time), { long: true })} gemutet!`)
-
-    /* setTimeout(function() {
+    setTimeout(function() {
       toMute.removeRole(muteRole)
       message.channel.send(`Endlich! ${toMute} wurde entmutet!`)
-    }, ms(time)) */
+    }, time)
   }
 }
 
