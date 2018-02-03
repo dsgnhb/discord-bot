0529df6b4fc1032ff4c3f979eb710bcccfc92c6a
Flo <x.client.ich@gmail.com>
2018-02-03 13:59:04 +0100

    Update show.js

--- commands/topdesign/show.js
+++ commands/topdesign/show.js
@@ -15,7 +15,7 @@ class Show extends TopDesignCommand {
 
   async run(message, args) {
     const postID = this.f.getPostID(args)
-    if (!postID) return message.channel.send('**TopDesign** | Nutze `!show #[Nr des Posts]` um einen Post zu löschen.')
+    if (!postID) return message.channel.send('**TopDesign** | Nutze `!show #[Nr des Posts]` um einen Post zu betrachten.')
     try {
       const request = await this.f.getPost(postID)
       if (!request) return message.channel.send(`**TopDesign** | Der Post mit der Nummer **#${postID}** konnte nicht gefunden werden.`)
