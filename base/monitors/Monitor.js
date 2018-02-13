95cf445b4c57ea49a97f5b54abe144556c79ae99
Alexander M <CreepPlaysDE@users.noreply.github.com>
2018-02-13 22:30:42 +0100

    Update Monitor.js

--- base/monitors/Monitor.js
+++ base/monitors/Monitor.js
@@ -2,14 +2,14 @@ class Monitor {
   constructor(client, options = { name = '', description = 'Keine Beschreibung.', category = 'Basics', dm = true, guild = true, maxPermLevel = '10' }) {
     this.client = client
     this.conf = { 
-      dm = options.dm,
-      guild = options.guild,
-      maxPermLevel = options.maxPermLevel
+      dm: options.dm,
+      guild: options.guild,
+      maxPermLevel: options.maxPermLevel
     }
     this.help = {
-      name = options.name,
-      description = options.description,
-      category = options.category
+      name: options.name,
+      description: options.description,
+      category: options.category
     }
   }
 }
