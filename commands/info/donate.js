f34b52205c73e36669c7967e9dd3eca808e080ca
Flo <x.client.ich@gmail.com>
2018-02-15 12:51:58 +0100

    !donate

---
+++ commands/info/donate.js
@@ -0,0 +1,22 @@
+const Command = require('../../base/commands/Command.js')
+
+
+class Donate extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'donate',
+            category: 'Info',
+            description: 'Unterstütze designhub',
+            usage: 'donate',
+            aliases: ['donations']
+        })
+    }
+
+    async run(message, args) {
+        message.channel.send("Wenn du designhub magst und uns unterstützen möchtest, um das Projekt aktiv zuhalten, freuen wir uns über deine Hilfe!" +
+            "\nAls Dankeschön erhälst du coole Belohnungen. Außerdem erwähnen wir dich auf unserer Website und bieten dir 'nen extra Rang auf unserem Discord an." +
+            "\n≫ https://designhub.fun/donate ")
+    }
+}
+
+module.exports = Donate
